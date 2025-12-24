f7d11c1816329d6f2e9b1090a743894fac5f5772
ajeetkumarAI <ajeetkumar.kumar.4434@gmail.com>
2025-12-24 18:28:16 +0530

    updated vectorstore

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,9 +1,18 @@
 from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 
-def create_vectorstore(chunks, embeddings, persist_dir="./chroma_db"):
-    return Chroma.from_documents(
+def create_vectorstore(chunks, embeddings):
+    # Create FAISS index directly from documents
+    vectorstore = FAISS.from_documents(
         documents=chunks,
-        embedding=embeddings,
-        persist_directory=persist_dir
+        embedding=embeddings
     )
+    return vectorstore
 
+
+# def create_vectorstore(chunks, embeddings, persist_dir="./chroma_db"):
+#     return Chroma.from_documents(
+#         documents=chunks,
+#         embedding=embeddings,
+#         persist_directory=persist_dir
+#     )
